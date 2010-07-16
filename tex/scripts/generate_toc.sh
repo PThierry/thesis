9df76158671c0cd11bcbb624af28f1d38f65f12e
t0090690 <t0090690@N00221504.fleuve.clb.tcfr.thales>
2010-07-16 13:22:12 +0200

    updated thesis: part generation instead of static list of subelement, using script shell. Updated content.

---
+++ tex/scripts/generate_toc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+parts=`find includes/fr -type d -name "part*" -exec basename {} \;`;
+cp document.tex.in document.tex
+
+for part in $parts; do
+	echo "* adding $part"
+	echo "% starting $part" >> document.tex
+	echo "\input{includes/fr/$part/title.tex}" >> document.tex
+	file_list=`find includes/fr/$part -name "*.tex" | grep -v "title.tex"`
+	for file in $file_list; do
+                echo "  adding file $file"
+		echo "\input{$file}" >> document.tex;
+	done
+done
+
+echo '
+\backmatter
+
+\input{includes/\lang/appendix/title.tex}
+
+\printglossary
+
+% bibliography
+\nocite{*}
+\bibliography{bibliography}
+\bibliographystyle{plain}
+
+\printindex
+
+\end{document}' >> document.tex
