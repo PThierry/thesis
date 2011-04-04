b0b2d6103498e136fcccde83bbe92fbdb1ee64b4
Philippe Thierry <phil@reseau-libre.net>
2011-04-04 19:24:39 +0200

    updated glossary to support glossaries new package. Update toc generator.

--- tex/scripts/generate_toc.sh
+++ tex/scripts/generate_toc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-parts=`find includes/fr -type d -name "part*" -exec basename {} \; | sort`;
+parts=`find includes/en -type d -name "part*" -exec basename {} \; | sort`;
 cp document.tex.in document.tex
 
 for part in $parts; do
 	echo "* adding $part"
 	echo "% starting $part" >> document.tex
-	echo "\input{includes/fr/$part/title.tex}" >> document.tex
-	file_list=`find includes/fr/$part -name "*.tex" | grep -v "title.tex" | sort`
+	echo "\input{includes/en/$part/title.tex}" >> document.tex
+	file_list=`find includes/en/$part -name "*.tex" | grep -v "title.tex" | sort`
 	for file in $file_list; do
                 echo "  adding file $file"
 		echo "\input{$file}" >> document.tex;
